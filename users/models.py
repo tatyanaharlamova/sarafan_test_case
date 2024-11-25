02865a4199bb52bf2ce973c73dc81b09879387e8
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-11-25 16:59:58 +0300

    Add authorisation

---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Укажите телефон",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Добавьте аватар",
+    )
+    city = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Укажите город",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
